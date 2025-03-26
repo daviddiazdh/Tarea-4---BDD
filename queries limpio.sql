e4a3863e51a9265b2460ef0949bf9ce5e54323c7
Jesus-0502 <jesusgutierrez4825@gmail.com>
2025-03-26 16:31:03 -0400

    agregar query 40

--- queries limpio.sql
+++ queries limpio.sql
@@ -565,6 +565,33 @@ JOIN (
     LIMIT 1
 ) q0 ON q0."codfs" = fs.codfs;
 
+-- 42. Las fuentes de soda que son frecuentadas por al menos dos  bebedores que le gustan al menos 3 de las bebidas que sirven.
+SELECT
+    q0."codfs",
+    fs.nombrefs
+FROM (
+    SELECT
+        g.ci AS "ci",
+        COUNT(DISTINCT v.codbeb) AS "codbeb",
+        v.codfs AS "codfs"
+    FROM gusta g
+    JOIN vende v ON g.codbeb = v.codbeb
+    GROUP BY
+        g.ci,
+        v.codfs
+    HAVING
+        COUNT(DISTINCT v.codbeb) >= 3
+    ORDER BY
+        g.ci
+) q0
+JOIN frecuenta f ON f.ci = q0."ci" AND f.codfs = q0."codfs"
+JOIN fuente_soda fs ON f.codfs = fs.codfs
+GROUP BY
+    q0."codfs",
+    fs.nombrefs
+HAVING
+    COUNT(DISTINCT q0."ci") >= 2;
+
 -- 44. Las fuentes de soda que no sirven bebidas que no le gustan a al menos uno de los bebedores que la frecuentan.
 SELECT 
     q0."codfs",
