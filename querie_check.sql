0334880522d4ee2d0985d94dcecd63b99c93b8ac
Jesus-0502 <jesusgutierrez4825@gmail.com>
2025-03-26 17:06:21 -0400

    agregar query 22

--- querie_check.sql
+++ querie_check.sql
@@ -1,35 +1,30 @@
--- 22. Los bebedores a quienes no les gusta bebida alguna pero frecuentan al menos una fuente de soda.
-SELECT
-    b.ci,
-    b.nombre
-FROM bebedor b
-JOIN frecuenta fr ON b.ci = fr.ci
-WHERE b.ci NOT IN (
-    SELECT 
-        b.ci
-    FROM bebedor b
-    JOIN gusta g ON b.ci = g.ci
-);
+-- 28. Las bebidas que se sirven en las fuentes de soda que son frecuentadas por las personas que no les gusta la malta.
 
-
-
-
-
-
-
-
-
--- SELECT 
---     *
--- FROM bebedor b
--- WHERE b.ci NOT IN (
---     SELECT DISTINCT
---         fr.ci
---     FROM frecuenta fr
---     JOIN vende v ON v.codfs = fr.codfs
---     WHERE (fr.ci, v.codbeb) NOT IN(
---         SELECT
---             *
---         FROM gusta
---     )
--- );
+SELECT DISTINCT
+    beb.codbeb,
+    beb.nombrebeb,
+    q0.ci
+FROM (
+    SELECT
+        *
+    FROM bebedor beb
+    WHERE NOT EXISTS(
+        SELECT 
+            1
+        FROM (
+            SELECT 
+                b.ci AS "cen_cedula",
+                b.nombre AS "cen_nombre"
+            FROM (  SELECT 
+                        *
+                    FROM bebida
+                    WHERE nombrebeb = 'Malta') s
+            JOIN gusta g ON g.codbeb = s.codbeb
+            JOIN bebedor b ON g.ci = b.ci 
+        ) 
+        WHERE beb.ci = "cen_cedula" AND beb.nombre = "cen_nombre"
+    )
+) q0 
+JOIN frecuenta fr ON q0.ci = fr.ci
+JOIN vende v ON fr.codfs = v.codfs
+JOIN bebida beb ON v.codbeb = beb.codbeb;
--- queries limpio.sql
+++ queries limpio.sql
@@ -410,6 +410,37 @@ FROM(
 ) q0
 JOIN bebida b ON b.codbeb = q0."codbeb";
 
+-- 28. Las bebidas que se sirven en las fuentes de soda que son frecuentadas por las personas que no les gusta la malta.
+
+SELECT DISTINCT
+    beb.codbeb,
+    beb.nombrebeb,
+    q0.ci
+FROM (
+    SELECT
+        *
+    FROM bebedor beb
+    WHERE NOT EXISTS(
+        SELECT 
+            1
+        FROM (
+            SELECT 
+                b.ci AS "cen_cedula",
+                b.nombre AS "cen_nombre"
+            FROM (  SELECT 
+                        *
+                    FROM bebida
+                    WHERE nombrebeb = 'Malta') s
+            JOIN gusta g ON g.codbeb = s.codbeb
+            JOIN bebedor b ON g.ci = b.ci 
+        ) 
+        WHERE beb.ci = "cen_cedula" AND beb.nombre = "cen_nombre"
+    )
+) q0 
+JOIN frecuenta fr ON q0.ci = fr.ci
+JOIN vende v ON fr.codfs = v.codfs
+JOIN bebida beb ON v.codbeb = beb.codbeb;
+
 -- 29. Las fuentes de soda que son frecuentadas por las personas que les gusta la malta y que frecuentan la fuente de soda "La Montaña".
 SELECT DISTINCT
     f.codfs,
